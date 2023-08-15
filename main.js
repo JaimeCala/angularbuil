b6885889596731155d4ee0df362ea81c7ef9022e
Jaime <jcalamani.m@gmail.com>
2023-08-15 15:40:28 -0400

    emp_db2

--- main.js
+++ main.js
@@ -2134,8 +2134,8 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    API_URL: 'http://167.172.157.67:3000/api',
-    API_URL_SOCKET: 'http://167.172.157.67:8181',
+    API_URL: 'http://159.65.42.246:3000/api',
+    API_URL_SOCKET: 'http://159.65.42.246:8181',
 };
 /*
  * For easier debugging in development mode, you can import the following file
